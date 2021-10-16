331b753e12be1ebcab4867e0f60e65b24e3ff8cd
Olivier <hi@olivierodo.me>
2021-10-16 22:49:19 +0700

    chore: change the customize strategy No more docker image No more server No more inline args Only one nice yml file

--- 404-links.js
+++ 404-links.js
@@ -1,53 +1,59 @@
 const $404 = require('./src')
 const path = require('path')
-const minimist = require('minimist')
+const fs = require('fs')
+const YAML = require('yaml')
 
-const argv = minimist(process.argv.slice(2), {
-  default: { 
-    'ignore-url': [],
-    'ignore-files': []
-  }
-});
 
 let options = {
+  configFile: path.resolve(process.cwd(), '.404-links.yml'),
   log: console.log,
-  folder: argv.folder,
+  folder: process.cwd(),
   ignore: {
-    urls: argv['ignore-url'],
-    files: argv['ignore-files']
+    urls: [],
+    files: []
   }
 }
 
-if (!Array.isArray(options.ignore.urls)) {
-  options.ignore.urls = [options.ignore.urls]
+
+options.log(`\n`)
+options.log(`😁  I'm so trilled to be here an support you on this task.`)
+options.log(`Let's take a look at the different links from your markdown files real quick!`)
+
+if (true === fs.existsSync(options.configFile)) {
+  const yaml = YAML.parse(fs.readFileSync(options.configFile).toString('utf-8'))
+  options = Object.assign(options, yaml)
+} else {
+  options.log(`> No config file has been found on the folder ${path.dirname(options.configFile)}`)
 }
+
+options.log('=====================================================')
+options.log(`> Folder to parse: ${options.folder}`)
+options.ignore.urls && options.log(`> Url to ignore: ${options.ignore.urls.length}`)
+options.ignore.files && options.log(`> File to ignore: ${options.ignore.files.length}`)
+options.log('=====================================================')
+
 
 options.ignore.urls.push('https://')
 options.ignore.urls.push('http://')
 
-if (!Array.isArray(options.ignore.files)) {
-  options.ignore.files = [options.ignore.files]
-}
-
-let stream = new $404(options)
-
-
+const stream = new $404(options)
 stream
   .on('data', (chunk) => {
-   const { status, passed, url } = JSON.parse(chunk.toString())
-   options.log(`> ${passed ? '✅':'❌'} - [${status}] - ${url}`)
+     const { status, passed, url } = JSON.parse(chunk.toString())
+     options.log(`> ${passed ? '✅':'❌'} - [${status}] - ${url}`)
   })
   .on('end', function() {
     options.log('=====================================================')
     const errors = this.errors
     if (this.errors.length) {
-      options.log(`${this.errors.length} Errors:`)
+      options.log(`> ${this.errors.length} Errors:`)
       errors.forEach(err => {
-        options.log(`${err.status} - ${err.url}`)
+        options.log(`   * ${err.status} - ${err.url}`)
       })
     } else {
-      options.log('> All links are reachable 🥳')
+      options.log('> All the links are reachable 🥳')
     }
+    options.log(`If you have any issue do not hesitate to open an issue on https://github.com/restqa/404-links`)
     process.exit(errors.length ? 1 : 0)
   })
 
